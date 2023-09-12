d14c7d91e351aa796e566af7927c188ec9a2c0ba
Chris Butler <butlertree@gmail.com>
2023-09-12 15:25:19 -0600

    Save button disabled function

---
+++ main.js
@@ -0,0 +1,27 @@
+var titleInput = document.querySelector('#title');
+var bodyInput = document.querySelector('#body');
+var saveButton = document.querySelector('button');
+
+titleInput.addEventListener('input', toggleSaveButton);
+bodyInput.addEventListener('input', toggleSaveButton);
+
+// toggleSaveButton();
+
+function toggleSaveButton() {
+    if (titleInput.value === '' || bodyInput.value === '') {
+        saveButton.classList.add('disabled')
+        saveButton.disabled = true
+    } else {
+        saveButton.classList.remove('disabled')
+        saveButton.disabled = false;
+    }
+}
+toggleSaveButton();
+
+
+
+
+
+
+
+
