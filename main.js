194ab7df5379baae0d4aba857b152f3065884cbd
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-09-12 16:24:47 -0400

    Add query selectors, data model and event listener on save button

---
+++ main.js
@@ -0,0 +1,11 @@
+var saveButton = document.querySelector("button");
+var titleText = document.querySelector("#title");
+var bodyText = document.querySelector("#body");
+var outputContainer = document.querySelector(".output-container");
+
+
+var ideas = {};
+
+saveButton.addEventListener("click", function() {
+    console.log("this button is clicked");
+})
